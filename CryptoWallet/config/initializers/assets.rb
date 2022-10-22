f330583aa76318c3146612c43e1610176ee770cc
DaianaSimao <daiana.xd11@hotmail.com>
2022-10-22 13:52:44 -0300

    Configurando o notify

--- CryptoWallet/config/initializers/assets.rb
+++ CryptoWallet/config/initializers/assets.rb
@@ -19,6 +19,8 @@ Rails.application.config.assets.precompile += %w(   coins.css
 
 Rails.application.config.assets.precompile += %w(  cable.js
                                                     coins.js
+                                                    jquery.js
                                                     mining_types.js
+                                                    notify.js
                                                     welcome.js
                                                 )
