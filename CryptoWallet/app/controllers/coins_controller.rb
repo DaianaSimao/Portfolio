7d9f32f6301b63adabb2c4cecc0e088b8cd4b0c1
DaianaSimao <daiana.xd11@hotmail.com>
2022-11-01 15:41:06 -0300

    Deploy do projeto do curso

--- CryptoWallet/app/controllers/coins_controller.rb
+++ CryptoWallet/app/controllers/coins_controller.rb
@@ -1,5 +1,5 @@
 class CoinsController < ApplicationController
-  before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_coin, only: [:show, :edit, :update, :destroy ]
   before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
   # GET /coins or /coins.json
