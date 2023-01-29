d6f374a9ec8e9170228bce912edcf38e60c88777
DaianaSimão <107774354+DaianaSimao@users.noreply.github.com>
2023-01-29 14:38:54 -0300

    Criando a parte de comentários dos post

---
+++ blog/app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+    def create
+        comment = Comment.create!(comments_params)
+
+        redirect_to comment.post, notice: "O seu comentario foi enviado com sucesso!"
+    end
+
+    private
+    
+    def comments_params
+        params.require(:comment).permit(:author, :body )
+    end
+end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "posts#index"
+
   resources :posts
+  resources :comments, only: %i[create]
   
 end
