c77bbf6ad515b4a37e16bb68fe7771fc607e0392
DaianaSimão <107774354+DaianaSimao@users.noreply.github.com>
2023-02-04 14:45:27 -0300

    Configurando o bootstrap no projeto

--- todo/app/javascript/packs/application.js
+++ todo/app/javascript/packs/application.js
@@ -7,5 +7,8 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap/dist/js/bootstrap.bundle";
+import "../src/application.css";
+
 Rails.start()
 ActiveStorage.start()
