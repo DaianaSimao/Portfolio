2627e62105a27028ca475c76c0976f610eea94e4
DaianaSimão <107774354+DaianaSimao@users.noreply.github.com>
2023-02-07 12:05:39 -0300

    Configurando o bootstrap para funcionar de forma correta no rails.

--- todo/app/javascript/packs/application.js
+++ todo/app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap/dist/js/bootstrap.bundle"
+import "../src/application.css"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
