341e9bf5e8d5736556ee9ac555a02fea3fe61123
DaianaSimão <107774354+DaianaSimao@users.noreply.github.com>
2023-02-07 11:41:09 -0300

    Deletando projeto

--- todo/app/models/task.rb
+++
@@ -1,5 +0,0 @@
-class Task < ApplicationRecord
-    validates :description,  presence: true
-
-    validates :done, inclusion: {in: [true, false]}
-end
--- todo/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  root "tasks#index"
-  resources :tasks, except: %i[show]
-end
