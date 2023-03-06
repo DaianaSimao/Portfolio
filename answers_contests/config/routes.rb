ed1fe0967f051e642c8cd1298bdd08028bc7ccd9
DaianaSimão <107774354+DaianaSimao@users.noreply.github.com>
2023-03-06 15:52:55 -0300

    Corrigindo os sistema. Trocando Tudo do profile para user.

--- answers_contests/config/routes.rb
+++ answers_contests/config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  namespace :profiles_backoffice do
+  
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
+  devise_for :users
 
-  root to: 'welcome#index'
+  get 'inicio', to: 'site/welcome#index'
+
+  root to: 'site/welcome#index'
 end
