3d248e8c5c2a5ab84deae9a2ee1446729d0aced4
DaianaSimao <daiana.xd11@hotmail.com>
2023-05-11 17:45:58 -0300

    Alterando a Senha do Administrador

--- answers_contests/app/controllers/admins_backoffice/admins_controller.rb
+++ answers_contests/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,12 +1,15 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
-    @admins = Admin.all
+    @admins = Admin.all.sort
   end
   def edit
     @admin = Admin.find(params[:id])
   end
 
   def update
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
     @admin = Admin.find(params[:id])
     params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
     if @admin.update(params_admin)
