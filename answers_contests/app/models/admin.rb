eb1bd28423fa9ff8a3343ac930898f1e09d8be65
DaianaSimao <daiana.xd11@hotmail.com>
2023-05-12 15:03:46 -0300

    Configurando a gem Kamari para paginação da aplicação

--- answers_contests/app/models/admin.rb
+++ answers_contests/app/models/admin.rb
@@ -3,4 +3,7 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Kamanari: gem paginadora para aplicação
+  paginates_per 5
 end
