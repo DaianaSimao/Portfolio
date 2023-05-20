cf4a2da75a76d3f77d9e3e27dcc01f9372d7cf0a
DaianaSimao <daiana.xd11@hotmail.com>
2023-05-20 12:13:42 -0300

    Filtrando perguntas por assunto

--- answers_contests/app/helpers/site_helper.rb
+++ answers_contests/app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Últimas perguntas cadastradas..."
+    when 'questions'
+      "Resultado para os termos \"#{params[:term]}\"..."
+    when 'subject'
+      "Mostrando perguntas para o assunto \"#{params[:subject]}\"...."
+    end
+  end
 end
--- answers_contests/app/models/question.rb
+++ answers_contests/app/models/question.rb
@@ -5,12 +5,20 @@ class Question < ApplicationRecord
 
   paginates_per 5
 
+  # SCOPE SEARCH
+  scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
+
   scope :_search_, ->(page, term){
-    includes(:answers)
+    includes(:answers, :subject)
     .where("lower(description) LIKE ?", "%#{term.downcase}%")
-    .page(page)}
+    .page(page)
+  }
 
   scope :last_questions, ->(page){
-    includes(:answers).order('created_at desc').page(page)
+    includes(:answers, :subject).order('created_at desc').page(page)
   }
 end
