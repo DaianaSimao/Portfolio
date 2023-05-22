d34516569e759dfec54efaa735569a155cb38b6e
DaianaSimao <daiana.xd11@hotmail.com>
2023-05-22 16:11:56 -0300

    Trocando a rota do administrador.

--- answers_contests/config/routes.rb
+++ answers_contests/config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'inicio', to: 'site/welcome#index'
+  get 'admin', to: 'admins_backoffice/welcome#index'
 
   root to: 'site/welcome#index'
 end
