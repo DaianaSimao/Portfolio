fddb7a404770a01caaea632b95237ed7373d486b
DaianaSimao <daiana.xd11@hotmail.com>
2023-05-24 11:43:23 -0300

    Adicionando foto do perfil (Active Storage)

--- answers_contests/app/controllers/users_backoffice/profile_controller.rb
+++ answers_contests/app/controllers/users_backoffice/profile_controller.rb
@@ -9,7 +9,9 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   def update
     if @user.update(params_user)
       bypass_sign_in(@user)
-      redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
+      unless params_user[:user_profile_attributes][:avatar]
+        redirect_to users_backoffice_welcome_index_path, notice: "Avatar atualizado com sucesso!"
+      end
     else
       render :edit
     end
@@ -22,7 +24,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
   def params_user
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-    user_profile_attributes: [:id, :address, :gender, :birthdate])
+    user_profile_attributes: [:id, :address, :gender, :birthdate, :avatar])
   end
 
   def verify_password
