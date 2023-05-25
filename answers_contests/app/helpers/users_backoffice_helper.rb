59086e76652dd15ce16e1e74512fbffe9809882d
DaianaSimao <daiana.xd11@hotmail.com>
2023-05-25 17:34:57 -0300

    Configurando o formulário para recuperção de senha do usuário.

--- answers_contests/app/helpers/users_backoffice_helper.rb
+++ answers_contests/app/helpers/users_backoffice_helper.rb
@@ -1,7 +1,8 @@
 module UsersBackofficeHelper
-  def avatar_url 
-    avatar = current_user.user_profile.avatar
-    avatar.attached? ? avatar : 'img.jpg'
+  
+  def avatar_url
+     avatar = current_user.user_profile.avatar
+     avatar.attached? ? avatar : 'img.jpg'
   end
-
+  
 end
