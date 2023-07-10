e158e702146fbf722c09a8513f7d1b3e6e17445e
DaianaSimao <daiana.xd11@hotmail.com>
2023-07-10 10:40:58 -0300

    Configurando o font awesome pra os icones

--- Estacionamneto_ParkHub/app/javascript/application.js
+++ Estacionamneto_ParkHub/app/javascript/application.js
@@ -2,7 +2,8 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "@fortawesome/fontawesome-free/js/all";
 
-
+//= require font-awesome
 //= require jquery
 //= require jquery_ujs
