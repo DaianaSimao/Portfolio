2628ac1c8a0c773e33c38d40e6254f2bf1eb4d28
DaianaSimao <daiana.xd11@hotmail.com>
2023-07-10 10:54:41 -0300

    configuro aws rotas do de

--- Estacionamneto_ParkHub/config/routes.rb
+++ Estacionamneto_ParkHub/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :estacionamentos
   resources :vagas
   resources :veiculos
   resources :precos
   resources :tickets
   get 'inicio/index'
-  devise_for :funcionarios
-  devise_for :admins
   root "inicio#index"
 end
