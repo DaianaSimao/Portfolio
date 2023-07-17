56575bd469ebcd58c1c92308e3de6f8946c04c19
DaianaSimao <daiana.xd11@hotmail.com>
2023-07-17 19:16:05 -0300

    Modifiquei as rotas para as views correspondentes.

--- Estacionamneto_ParkHub/config/routes.rb
+++ Estacionamneto_ParkHub/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :alugueis
+
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :estacionamentos
+  resources :checkins
   resources :vagas
   resources :veiculos
   resources :precos
+
   get 'inicio/index'
   root "inicio#index"
+
 end
